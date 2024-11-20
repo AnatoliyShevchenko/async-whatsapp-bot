00518cb48fce3a59bc04bf751b7ffe8230dc6ea4
AnatoliyShevchenko <genitalgrinder90@outlook.com>
2024-11-20 20:35:20 +0500

    clean up

--- test.py
+++ test.py
@@ -9,10 +9,7 @@ TOKEN = "your token"
 
 async def main():
     bot = Whatsapp(number_id=NUMBER_ID, token=TOKEN)
-    # temp = await bot.send_text_message(
-    #     recipient_phone_number="number",
-    #     text_message="Привет из питона!"
-    # )
+
     temp = await bot.send_sample_text_message(
         recipient_phone_number="number",
         sample_name="agreement"
